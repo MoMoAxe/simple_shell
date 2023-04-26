f6ee1475c59b294db9d8e92cccc92b9af9d7cf0f
emmyq10 <emmatunde2001@gmail.com>
2023-04-26 06:40:59 -0700

    free.c

---
+++ free.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * free_dp - function for freeing all  memory allocated to command
+ * @command: allocated memory to free.
+ * Return: Null
+ */
+void free_dp(char **command)
+{
+	size_t i = 0;
+
+	if (command == NULL)
+		return;
+
+	while (command[i])
+	{
+		free(command[i]);
+		i++;
+	}
+
+	if (command[i] == NULL)
+		free(command[i]);
+	free(command);
+}
+
+
+/**
+ * free_exit - frees all the memory allocated and exit
+ * @command: allocated command memory to free
+ * Return: Null
+ */
+void free_exit(char **command)
+{
+	size_t i = 0;
+
+	if (command == NULL)
+		return;
+
+	while (command[i])
+	{
+		free(command[i]);
+		i++;
+	}
+
+	if (command[i] == NULL)
+		free(command[i]);
+	free(command);
+	exit(EXIT_FAILURE);
+}
