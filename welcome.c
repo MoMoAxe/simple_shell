ebe5478febcfdb53774759c4ea8c5536cdfb9352
MoMoAxe <tobioluwa348@gmail.com>
2023-04-26 04:51:24 -0700

    welcome.c

---
+++ welcome.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CMD 256
+
+/**
+ * welcome - Welcome message
+ * @name: pointer to the name string
+ * Return: void
+ */
+void welcome(char *name)
+{
+	int i = 0;
+	char hlder[MAX_CMD];
+	int j;
+
+	j = 0;
+
+	while (i < strlen(name))
+	{
+		if (name[i] != ' ')
+		hlder[j] = name[i];
+		else
+		break;
+		j++;
+		i++;
+	}
+
+	hlder[j] = '\0';
+
+	printf("%s welcome to ALX where we do hard things\n", hlder);
+}
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char buff[MAX_CMD];
+
+	printf("Enter Your First name and Surname:\n");
+	fgets(buff, MAX_CMD, stdin);
+	welcome(buff);
+
+	return (0);
+}
